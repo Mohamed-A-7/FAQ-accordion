0eb4712530933a95488ebbf3c8250513ff18b7a4
Mohamed Ali <152911704+Mohamed-A-7@users.noreply.github.com>
2024-11-02 14:12:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 let buttons = document.querySelectorAll(".btn");
 let answers = document.querySelectorAll(".question-answer");
 
+answers[0].classList.remove("hidden");
+buttons[0].setAttribute("src", "./assets/images/icon-minus.svg");
+
 buttons.forEach((btn, index) => {
+  
   btn.addEventListener("click", () => {
+    
     answers.forEach((ans, i) => {
+      
       ans.classList.add("hidden");
       buttons[i].setAttribute("src", "./assets/images/icon-plus.svg");
+      
     });
 
     answers[index].classList.toggle("hidden");
 
     btn.classList.contains("hidden")
+      
       ? btn.setAttribute("src", "./assets/images/icon-plus.svg")
+      
       : btn.setAttribute("src", "./assets/images/icon-minus.svg");
   });
 });
